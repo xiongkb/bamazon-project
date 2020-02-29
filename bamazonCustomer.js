2c48d3baa8b3be75d60ae440c2c48341eb185fd6
xiongkb <mkan-bx@hotmail.com>
2020-02-28 21:53:58 -0800

    Added closing store inquirer code and prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,10 +16,11 @@ connection.connect(function(err) {
 });
 
 function showDB() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        for (var i = 0; i < res.length; i++) {
+            console.log(JSON.parse(JSON.stringify(res[i])));
+        }
     });
     question();
 };
@@ -29,13 +30,43 @@ function question() {
         {
             name: "todo",
             type: "list",
-            message: "What would you like to do",
+            message: "What would you like to do?",
             choices: [
                 "Buy a product",
                 "Nothing"
             ]
-
-
         }
-    )
+    ).then(function(answer) {
+        switch (answer.todo) {
+            case "Buy a product":
+                shop();
+                break;
+            case "Nothing":
+                close();
+                break;
+        }
+    });
+};
+
+function close() {
+    console.log("Thank you for shopping with Bamazon!\nCome again next time!");
+    connection.end();
 }
+
+function shop() {
+    inquirer.prompt(
+        {
+            name: "buy",
+            type: "input",
+            message: "What product would you like to buy?"
+        },
+        {
+            name: "item",
+            type: "input",
+            message: "How many units would you like to buy?"
+        }
+    ).then(function(answer) {
+        console.log(answer.buy);
+        console.log(answer.item);
+    });
+};
