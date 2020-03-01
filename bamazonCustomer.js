09732657702c037f1baedbb625ca47bdfed9c8ea
xiongkb <mkan-bx@hotmail.com>
2020-02-29 19:24:03 -0800

    added code for update and checking stock qty

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,12 +17,14 @@ connection.connect(function(err) {
 
 function showDB() {
     connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+        console.log("-------- Welcome to Bamazon! --------\nHere is a list of items that's available for purchase:\n");
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(JSON.parse(JSON.stringify(res[i])));
         }
-    });
-    question();
+        console.log("-------------------------\n")
+        question();
+    }); 
 };
 
 function question() {
@@ -51,22 +53,62 @@ function question() {
 function close() {
     console.log("Thank you for shopping with Bamazon!\nCome again next time!");
     connection.end();
-}
+};
 
 function shop() {
-    inquirer.prompt(
-        {
-            name: "buy",
-            type: "input",
-            message: "What product would you like to buy?"
-        },
+    inquirer.prompt([
         {
             name: "item",
             type: "input",
-            message: "How many units would you like to buy?"
+            message: "What product would you like to buy? Enter the item_id: ",
+            validate: function(value) {
+                if (isNaN(value) ===false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "unit",
+            type: "input",
+            message: "How many units would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) ===false) {
+                    return true;
+                }
+                return false;
+            }
         }
-    ).then(function(answer) {
-        console.log(answer.buy);
-        console.log(answer.item);
+    ]).then(function(answer) {
+        console.log("You are buying # " + answer.item);
+        var query = "SELECT * FROM products WHERE item_id = ?";
+        connection.query(query, [answer.item], function(err, res) {
+            if (err) throw err;
+            if (res[0].stock_qty > 0) {
+                updateDB(answer.item, answer.unit);
+            } else {
+                console.log("There are not enough items in stock!\nPlease change your QTY or select new product!")
+                shop();
+            };
+        });
     });
 };
+
+function updateDB(item, unit) {
+    console.log("==========You reached updateDB!==========")
+    var query = "UPDATE products SET stock_qty = (stock_qty - ?) WHERE item_id = ?";
+    var data = [unit, item];
+    connection.query(query, data, function(err, res) {
+        if (err) throw err;
+        purchase(item);
+    });
+};
+
+function purchase(itemID) {
+    var query = "SELECT * FROM products WHERE item_id = ?";
+    var entry = itemID;
+    connection.query(query, entry, function(err, res) {
+        console.log(res);
+    })
+    connection.end();
+}
